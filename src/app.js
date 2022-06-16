bbd31490e34e8b4a8783281c9dbf869a548cdd74
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2022-06-16 00:39:54 -0300

    feature: (post /sing-up and /tweets, get /tweets) endpoints implemented

--- src/app.js
+++ src/app.js
@@ -3,6 +3,37 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+const users = [
+  {
+    username: "bobesponja",
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+  },
+];
+
+const tweets = [
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+];
+
+app.post("/sing-up", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+  const tweet = req.body;
+  tweets.push(tweet);
+  res.send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+  res.send(tweets.slice(-10));
+});
 
 app.listen(5000, () => {
   console.log(`listening on port 5000`);
