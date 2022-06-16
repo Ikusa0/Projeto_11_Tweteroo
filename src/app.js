28f3521192b294ed5e7d20097dae45415f19baa5
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2022-06-16 13:04:18 -0300

    feature: getting tweets now have pagination with query string "?page=x"

--- src/app.js
+++ src/app.js
@@ -15,7 +15,123 @@ const users = [
 const tweets = [
   {
     username: "bobesponja",
-    tweet: "eu amo o hub",
+    tweet: "1",
+  },
+  {
+    username: "bobesponja",
+    tweet: "2",
+  },
+  {
+    username: "bobesponja",
+    tweet: "3",
+  },
+  {
+    username: "bobesponja",
+    tweet: "4",
+  },
+  {
+    username: "bobesponja",
+    tweet: "5",
+  },
+  {
+    username: "bobesponja",
+    tweet: "6",
+  },
+  {
+    username: "bobesponja",
+    tweet: "7",
+  },
+  {
+    username: "bobesponja",
+    tweet: "8",
+  },
+  {
+    username: "bobesponja",
+    tweet: "9",
+  },
+  {
+    username: "bobesponja",
+    tweet: "10",
+  },
+  {
+    username: "bobesponja",
+    tweet: "11",
+  },
+  {
+    username: "bobesponja",
+    tweet: "12",
+  },
+  {
+    username: "bobesponja",
+    tweet: "13",
+  },
+  {
+    username: "bobesponja",
+    tweet: "14",
+  },
+  {
+    username: "bobesponja",
+    tweet: "15",
+  },
+  {
+    username: "bobesponja",
+    tweet: "16",
+  },
+  {
+    username: "bobesponja",
+    tweet: "17",
+  },
+  {
+    username: "bobesponja",
+    tweet: "18",
+  },
+  {
+    username: "bobesponja",
+    tweet: "19",
+  },
+  {
+    username: "bobesponja",
+    tweet: "20",
+  },
+  {
+    username: "bobesponja",
+    tweet: "21",
+  },
+  {
+    username: "bobesponja",
+    tweet: "22",
+  },
+  {
+    username: "bobesponja",
+    tweet: "23",
+  },
+  {
+    username: "bobesponja",
+    tweet: "24",
+  },
+  {
+    username: "bobesponja",
+    tweet: "25",
+  },
+  {
+    username: "bobesponja",
+    tweet: "26",
+  },
+  {
+    username: "bobesponja",
+    tweet: "27",
+  },
+  {
+    username: "bobesponja",
+    tweet: "28",
+  },
+  {
+    username: "bobesponja",
+    tweet: "29",
+  },
+  {
+    username: "bobesponja",
+    tweet: "30",
   },
 ];
 
@@ -32,7 +148,15 @@ app.post("/tweets", (req, res) => {
 });
 
 app.get("/tweets", (req, res) => {
-  res.send(tweets.slice(-10));
+  const page = parseInt(req.query.page);
+  if (!page || page < 1) {
+    res.status(400).send("Informe uma página válida!");
+  }
+  if (page === 1) {
+    res.send(tweets.slice(-10).reverse());
+  } else {
+    res.send(tweets.slice(-10 * page, -10 * page + 10).reverse());
+  }
 });
 
 app.get("/tweets/:username", (req, res) => {
